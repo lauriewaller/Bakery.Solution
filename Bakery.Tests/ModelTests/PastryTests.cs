645029c25095731ebac4f9377df816763c4da284
Laurie Waller <lauriewaller7@gmail.com>
2021-04-23 19:44:19 -0700

    create good fail for Pastry constructor

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceofPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}

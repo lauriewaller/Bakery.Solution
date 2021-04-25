ac923a36ea133cc1fb0319e1cace7b513c995bcc
Laurie Waller <lauriewaller7@gmail.com>
2021-04-24 23:44:57 -0700

    remove comments

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -29,6 +29,3 @@ namespace Bakery.Models
 
   }
 }
-
-// constructor test 
-// 
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,7 +11,7 @@ namespace UserBakery
       Bread breadToday = new Bread();
       Pastry pastryToday = new Pastry();
 
-      Console.WriteLine("Welcome to Pierre's Bakery! Today we're offering sourdough bread($" + breadToday.Price + ")" + " and a puff pastry($" + pastryToday.Price + ").");
+      Console.WriteLine("Welcome to Pierre's Bakery! Today we're offering sourdough bread($" + breadToday.Price + ")" + " and puff pastries($" + pastryToday.Price + ").");
       Console.WriteLine("Specials Today: Buy 2 loaves of bread, get 1 free! Get 3 pastries for just $5!");
       Console.WriteLine("Enter how many loaves of sourdough bread you would like:");
       string breadEntry = Console.ReadLine();
@@ -19,11 +19,6 @@ namespace UserBakery
       Console.WriteLine("Enter how many puff pastries you would like:");
       string pastryEntry = Console.ReadLine();
       int pastryNumber = int.Parse(pastryEntry);
-
-      int breadResult = breadToday.DetermineBreadPrice(breadNumber); //delete
-      int pastryResult = pastryToday.DeterminePastryPrice(pastryNumber); //delete
-      Console.WriteLine("bread: " + breadResult); //delete
-      Console.WriteLine("pastry: " + pastryResult); //delete
 
       int totalCost = breadToday.DetermineBreadPrice(breadNumber) + pastryToday.DeterminePastryPrice(pastryNumber);
 
